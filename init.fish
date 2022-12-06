4c26d5dc9db90dbd02cacd5d29e1e93536328234
Tom M <tom@tmewett.com>
2022-12-06 14:00:37 +0000

    Add -g back, wasn't redundant at all lol

--- init.fish
+++ init.fish
@@ -2,14 +2,14 @@ set PATH $path/bin $PATH
 
 
 # TERMCMD is used by Ranger to open a terminal.
-set -x TERMCMD in-terminal
+set -gx TERMCMD in-terminal
 
 
 # -j 0.5 - search results appear in middle of screen
 # -R - don't escape colours and text effects
 # -P prompt - the default medium/-m prompt, modified to always show the file
 #   (not just on first prompt) and appended with help info like 'man'
-set -x LESS "-j 0.5\$ -R -P ?f%f :- .?m(%T %i of %m) .?e(END) ?x- Next\: %x.:?pB%pB\%:byte %bB?s/%s...%t (press h for help or q to quit)\$ $LESS"
+set -gx LESS "-j 0.5\$ -R -P ?f%f :- .?m(%T %i of %m) .?e(END) ?x- Next\: %x.:?pB%pB\%:byte %bB?s/%s...%t (press h for help or q to quit)\$ $LESS"
 
 
 if status is-interactive
@@ -38,53 +38,53 @@ end
 ### Variable defaults ###
 
 if not set -q NUI_EDITOR
-    set NUI_EDITOR xdg-open
+    set -g NUI_EDITOR xdg-open
 end
 
 if not set -q NUI_TERMINAL_EDITOR
     if type -q nano
-        set NUI_TERMINAL_EDITOR nano
+        set -g NUI_TERMINAL_EDITOR nano
     else if type -q vi
-        set NUI_TERMINAL_EDITOR vi
+        set -g NUI_TERMINAL_EDITOR vi
     end
 end
 
 if not set -q NUI_OPENER
     if type -q rifle
-        set NUI_OPENER rifle
+        set -g NUI_OPENER rifle
     else if type -q open
-        set NUI_OPENER open
+        set -g NUI_OPENER open
     else if type -q xdg-open
-        set NUI_OPENER xdg-open
+        set -g NUI_OPENER xdg-open
     end
 end
 
 if not set -q NUI_RM_TRASHES
-    set NUI_RM_TRASHES true
+    set -g NUI_RM_TRASHES true
 end
 
 if not set -q NUI_H_NEW_TERMINAL
-    set NUI_H_NEW_TERMINAL true
+    set -g NUI_H_NEW_TERMINAL true
 end
 
 if not set -q NUI_TERMINAL
     if type -q exo-open
-        set NUI_TERMINAL exo-open --launch TerminalEmulator
+        set -g NUI_TERMINAL exo-open --launch TerminalEmulator
     else if type -q gnome-terminal
-        set NUI_TERMINAL gnome-terminal --
+        set -g NUI_TERMINAL gnome-terminal --
     else if type -q kgx  # New GNOME console
-        set NUI_TERMINAL kgx --
+        set -g NUI_TERMINAL kgx --
     end
 end
 
 if not set -q NUI_TERMINAL_CMD
-    set NUI_TERMINAL_CMD $NUI_TERMINAL
+    set -g NUI_TERMINAL_CMD $NUI_TERMINAL
 end
 
 if test (uname -s) = Darwin
-    set _NUI_MACOS
+    set -g _NUI_MACOS
 end
 
 if set -q _NUI_MACOS; or set -q DISPLAY
-    set _NUI_GRAPHICAL
+    set -g _NUI_GRAPHICAL
 end
